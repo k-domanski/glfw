0392a74a782c20797c19b8982ec8b825cf140428
k-dom <224285@edu.p.lodz.pl>
2021-03-22 15:31:04 +0100

    fixed premake and ignore

--- premake5.lua
+++ premake5.lua
@@ -3,8 +3,8 @@ project "GLFW"
     language "C"
     staticruntime "on"
 
-    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
     files
     {
